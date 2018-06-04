0cca1011453085ca9cb2faee505eadbfe994db43
Matt <mguest55@yahoo.com>
2018-06-03 20:51:50 -0700

    Initial Commit

---
+++ Grade HawkTests/Grade_HawkTests.swift
@@ -0,0 +1,36 @@
+//
+//  Grade_HawkTests.swift
+//  Grade HawkTests
+//
+//  Created by Matt Guest on 6/3/18.
+//  Copyright © 2018 AlphaApplications. All rights reserved.
+//
+
+import XCTest
+@testable import Grade_Hawk
+
+class Grade_HawkTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
