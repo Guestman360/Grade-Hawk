86727df8408576eaabbdaedd2451bf532134f5e7
Matt <mguest55@yahoo.com>
2018-06-24 11:51:23 -0700

    Added file groups to controllers and added one file for first view encountered.

---
+++ Grade Hawk/Controllers/Class/ClassesTableViewController.swift
@@ -0,0 +1,9 @@
+//
+//  ClassesTableViewController.swift
+//  Grade Hawk
+//
+//  Created by Matt Guest on 6/24/18.
+//  Copyright © 2018 AlphaApplications. All rights reserved.
+//
+
+import Foundation
