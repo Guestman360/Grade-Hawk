d3915e464ad7f2b9211414f45427de3539c4dfca
Matt <mguest55@yahoo.com>
2018-06-24 17:00:49 -0700

    Had to create a custom view for the calculate view.

---
+++ Grade Hawk/Custom Controls/UICalculateView/UICalculateView.swift
@@ -0,0 +1,200 @@
+//
+//  UICalculateView.swift
+//  Grade Hawk
+//
+//  Created by Matt Guest on 6/24/18.
+//  Copyright © 2018 AlphaApplications. All rights reserved.
+//
+
+import UIKit
+
+/// Delegate for the calculate view
+protocol UICalculateViewDelegate: class {
+    func calculateWasTapped(for: UICalculateView, score: String, total: String)
+    func exitButtonWasTapped(for: UICalculateView)
+}
+
+/// A view which calculates a percentage given the score and total
+open class UICalculateView: UIView {
+    
+    weak var delegate: UICalculateViewDelegate?
+    
+    // MARK: - Initalizers & Overrides
+    
+    public override init(frame: CGRect) {
+        super.init(frame: frame)
+        self.initialize()
+    }
+    
+    required public init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    // MARK: - Helper Methods
+    
+    private func initialize() {
+        
+        self.backgroundColor = UIColor(red: 0.878, green: 0.890, blue: 0.865, alpha: 1.0)
+        self.translatesAutoresizingMaskIntoConstraints = false
+        self.layer.cornerRadius = 5
+        self.clipsToBounds = true
+        // Add subviews
+        // Set frames for subviews
+        self.addSubview(headerView)
+        
+        let frameForScore = CGRect(x: 10, y: headerView.frame.maxY + 25, width: self.bounds.width - 20, height: 30)
+        scoreField.frame = frameForScore
+        self.addSubview(scoreField)
+        
+        let frameForSeperator = CGRect(x: 0, y: frameForScore.maxY + 10, width: self.bounds.width, height: seperatorLabel.frame.height)
+        seperatorLabel.frame = frameForSeperator
+        self.addSubview(seperatorLabel)
+        
+        let frameForTotal = CGRect(x: 10, y: frameForSeperator.maxY + 10, width: self.bounds.width - 20, height: 30)
+        totalField.frame = frameForTotal
+        self.addSubview(totalField)
+        
+        calculateButton.frame = CGRect(x: 0, y: self.bounds.maxY - 50, width: self.bounds.width, height: 50)
+        self.addSubview(calculateButton)
+        
+        // Add a border to view
+        let bottomBorder = CALayer()
+        bottomBorder.frame = CGRect(x: 0, y: titleLabel.frame.maxY + 5, width: self.layer.frame.width, height: 1)
+        bottomBorder.backgroundColor = UIColor(red: 0.337, green: 0.384, blue: 0.439, alpha: 1.00).cgColor
+        self.layer.addSublayer(bottomBorder)
+    }
+    
+    
+    // MARK: - Actions
+    
+    @objc func calculateButtonTapped(button: UIButton) {
+        button.animateWithPulse(withDuration: 0.3) { [weak self] in
+            guard let `self` = self else { return }
+            let score = self.scoreField.text ?? ""
+            let total = self.totalField.text ?? ""
+            self.delegate?.calculateWasTapped(for: self, score: score, total: total)
+        }
+    }
+    
+    @objc func exitButtonTapped(button: UIButton) {
+        self.endEditing(true)
+        self.delegate?.exitButtonWasTapped(for: self)
+    }
+    
+    // MARK: - Subviews
+    
+    /// The enclosing header view, encapsulates the title label and the exit button
+    open lazy var headerView: UIView = {
+        let view = UIView()
+        view.frame = CGRect(x: 0, y: 0, width: self.bounds.width, height: 30)
+        
+        self.titleLabel.frame = CGRect(x: 0, y: 0, width: view.bounds.width, height: view.bounds.height)
+        view.addSubview(self.titleLabel)
+        
+        self.exitButton.frame = CGRect(x: view.bounds.width - 40, y: 3, width: 25, height: 25)
+        view.addSubview(self.exitButton)
+        
+        return view
+    }()
+    
+    /// The title label which will be displayed ontop of the view
+    open lazy var titleLabel: UILabel = {
+        let label = UILabel()
+        label.text = "Calculate Percentage"
+        label.font = UIFont.systemFont(ofSize: 20)
+        label.textColor = UIColor(red: 0.337, green: 0.384, blue: 0.439, alpha: 1.00)
+        label.textAlignment = .center
+        return label
+    }()
+    
+    /// The exit button for the view
+    open lazy var exitButton: UIButton = {
+        let button = UIButton(type: .custom)
+        button.setImage(#imageLiteral(resourceName: "DeleteIcon"), for: .normal)
+        button.contentMode = .scaleAspectFit
+        button.tintColor = UIColor(red: 0.647, green: 0.576, blue: 0.878, alpha: 1.00)
+        button.addTarget(self, action: #selector(self.exitButtonTapped), for: .touchUpInside)
+        return button
+    }()
+    
+    /// The score UISafeTextField view
+    open lazy var scoreField: UISafeTextField = {
+        let config = NumberConfiguration(allowsSignedNumbers: false)
+        let field = UISafeTextField(frame: .zero, fieldType: .number, configuration: config)
+        field.placeholder = "Score"
+        field.tintColor = UIColor(red: 0.647, green: 0.576, blue: 0.878, alpha: 1.00)
+        field.font = UIFont.systemFont(ofSize: 18)
+        field.borderStyle = .roundedRect
+        field.keyboardType = .numbersAndPunctuation
+        field.returnKeyType = .next
+        field.attributedPlaceholder = NSAttributedString(string: "Score",
+                                                         attributes: [.foregroundColor: UIColor.frenchGray])
+        field.delegate = self
+        return field
+    }()
+    
+    /// The label in between the score and total fields
+    open lazy var seperatorLabel: UILabel = {
+        let label = UILabel()
+        let attrs: [NSAttributedStringKey: Any] = [.foregroundColor: UIColor.frenchGray,
+                                                   .font: UIFont.systemFont(ofSize: 15)]
+        label.attributedText = NSAttributedString(string: "OUT OF", attributes: attrs)
+        label.textAlignment = .center
+        label.resizeToFitText()
+        return label
+    }()
+    
+    /// The total UISafeTextField view
+    open lazy var totalField: UISafeTextField = {
+        let config = NumberConfiguration(allowsSignedNumbers: false)
+        let field = UISafeTextField(frame: .zero, fieldType: .number, configuration: config)
+        field.placeholder = "Total"
+        field.borderStyle = .roundedRect
+        field.tintColor = UIColor(red: 0.647, green: 0.576, blue: 0.878, alpha: 1.00)
+        field.font = UIFont.systemFont(ofSize: 18)
+        field.returnKeyType = .done
+        field.keyboardType = .numbersAndPunctuation
+        field.attributedPlaceholder = NSAttributedString(string: "Total",
+                                                         attributes: [.foregroundColor: UIColor.frenchGray])
+        field.delegate = self
+        return field
+    }()
+    
+    /// The button which the user will click to calculate the percentage
+    open lazy var calculateButton: UIButton = {
+        let button = UIButton(type: .custom)
+        let attrs: [NSAttributedStringKey: Any] = [.foregroundColor: UIColor.lightText,
+                                                   .font: UIFont.systemFont(ofSize: 18)]
+        button.layer.backgroundColor = UIColor(red: 0.647, green: 0.576, blue: 0.878, alpha: 1.00).cgColor
+        button.clipsToBounds = true
+        button.setAttributedTitle(NSAttributedString(string: "Calculate", attributes: attrs), for: .normal)
+        button.addTarget(self, action: #selector(self.calculateButtonTapped), for: .touchUpInside)
+        return button
+    }()
+}
+
+// MARK: - UITextField Delegation
+
+extension UICalculateView: UITextFieldDelegate {
+    public func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        
+        if textField !== scoreField {
+            totalField.resignFirstResponder()
+            return true
+        }
+        
+        scoreField.resignFirstResponder()
+        totalField.becomeFirstResponder()
+        
+        return true
+    }
+    
+    public func textFieldDidEndEditing(_ textField: UITextField) {
+        textField.resignFirstResponder()
+    }
+    
+    public func textField(_ textField: UITextField, shouldChangeCharactersIn range: NSRange, replacementString string: String) -> Bool {
+        guard let field = textField as? UISafeTextField else { return true }
+        return field.shouldChangeTextAfterCheck(text: string)
+    }
+}
---
+++ Grade Hawk/Custom Controls/UICalculateView/UICalculateViewController.swift
@@ -0,0 +1,163 @@
+//
+//  UICalculateViewController.swift
+//  Grade Hawk
+//
+//  Created by Matt Guest on 6/24/18.
+//  Copyright © 2018 AlphaApplications. All rights reserved.
+//
+
+import UIKit
+
+open class UICalculateViewController: UIBlurViewController {
+    
+    /// The percent of the calculated view
+    public typealias CalculateViewControllerCompletion = (Double) -> Void
+    
+    /// The completion for the calculate view, will be called when calculate button is pressed
+    open var calculationCompletion: CalculateViewControllerCompletion
+    
+    /// The calculate view instance
+    open lazy var calculateView: UICalculateView = {
+        let view = UICalculateView(frame: CGRect(origin: self.view.center, size: CGSize(width: 310, height: 210)))
+        view.delegate = self
+        return view
+    }()
+    
+    private var calcViewYConstraint: NSLayoutConstraint?
+    private var calcViewInitialY: CGFloat?
+    
+    // MARK: Initializers
+    
+    required public init(completion: @escaping CalculateViewControllerCompletion) {
+        self.calculationCompletion = completion
+        super.init(nibName: nil, bundle: nil)
+        self.modalPresentationStyle = .overCurrentContext
+    }
+    
+    required public init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    override open func viewDidLoad() {
+        super.viewDidLoad()
+        
+        self.view.addSubview(calculateView)
+        calculateView.center = self.view.center
+        
+        let width = self.view.frame.width > 310 ? 310 : self.view.frame.width - 15
+        let height: CGFloat = 210.0
+        // Add constraints
+        let widthConstraint = NSLayoutConstraint(item: calculateView, attribute: .width, relatedBy: .equal, toItem: nil,
+                                                 attribute: .notAnAttribute, multiplier: 1.0, constant: width)
+        let heightConstraint = NSLayoutConstraint(item: calculateView, attribute: .height, relatedBy: .equal, toItem: nil,
+                                                  attribute: .notAnAttribute, multiplier: 1.0, constant: height)
+        let xConstraint = NSLayoutConstraint(item: calculateView, attribute: .centerX, relatedBy: .equal, toItem: self.view,
+                                             attribute: .centerX, multiplier: 1.0, constant: 0)
+        self.calcViewYConstraint = NSLayoutConstraint(item: calculateView, attribute: .centerY, relatedBy: .equal, toItem: self.view,
+                                             attribute: .centerY, multiplier: 1.0, constant: 0)
+        NSLayoutConstraint.activate([widthConstraint, heightConstraint, xConstraint, self.calcViewYConstraint!])
+        
+        // Add recognizer to view
+        let tap = UITapGestureRecognizer(target: self, action: #selector(viewWasTapped))
+        self.view.addGestureRecognizer(tap)
+        
+        // Register for keyboard notifications
+        // If not on iPad where the view will be presented as a popover
+        if !(UIDevice.current.userInterfaceIdiom == .pad) {
+            // Setup keyboard notifications
+            NotificationCenter.default.addObserver(self,
+                                                   selector: #selector(keyboardDidShow),
+                                                   name: .UIKeyboardDidShow, object: nil)
+            
+            NotificationCenter.default.addObserver(self,
+                                                   selector: #selector(keyboardWillHide),
+                                                   name: .UIKeyboardWillHide, object: nil)
+        }
+    }
+    
+    open override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        self.calcViewInitialY = self.calcViewYConstraint?.constant
+    }
+    
+    open override func viewWillDisappear(_ animated: Bool) {
+        super.viewWillDisappear(animated)
+        // Remove notifications for keyboard
+        NotificationCenter.default.removeObserver(self)
+    }
+    
+    // MARK: Actions
+    
+    @objc func viewWasTapped(recognizer: UITapGestureRecognizer) {
+        let tapPoint = recognizer.location(in: self.view)
+        if calculateView.frame.contains(tapPoint) { return }
+        else { self.animateOut(completion: nil) }
+    }
+    
+    /// Called whenever keyboard is shown, adjusts scroll view
+    @objc func keyboardDidShow(notification: Notification) {
+        let userInfo = notification.userInfo!
+        let keyboardFrame: CGRect = (userInfo[UIKeyboardFrameEndUserInfoKey] as! NSValue).cgRectValue
+        
+        if (self.calculateView.frame.maxY) >= keyboardFrame.minY {
+            // Calculate view is covered adjust height
+            self.calcViewYConstraint?.constant -= (self.calculateView.frame.maxY - keyboardFrame.minY) + 10
+        }
+    }
+    
+    /// Called whenever keyboard is shown, adjusts scroll view
+    @objc func keyboardWillHide(notification: Notification) {
+        // Revert constraint
+        self.calcViewYConstraint?.constant = self.calcViewInitialY ?? self.view.center.y
+    }
+    
+    // MARK: Animations
+    
+    public override func animateIn(completion: UIBlurViewController.BlurViewAnimationCompletion?) {
+        super.animateIn(completion: completion)
+        // Custom animation code for calculate view
+        self.calculateView.alpha = 0.0
+        self.calculateView.transform = CGAffineTransform.init(scaleX: 0.2, y: 0.2)
+        UIView.animate(withDuration: animationDuration, delay: 0.0, usingSpringWithDamping: 1.0, initialSpringVelocity: 10,
+                       options: [.beginFromCurrentState, .curveEaseOut],
+                       animations: {
+                        self.calculateView.alpha = 1.0
+                        self.calculateView.transform = CGAffineTransform.init(scaleX: 1.0, y: 1.0)
+        }) { finished in
+            if finished { completion?(finished) }
+        }
+    }
+    
+    public override func animateOut(completion: UIBlurViewController.BlurViewAnimationCompletion?) {
+        super.animateOut(completion: completion)
+        // Custom animation code for calculate view
+        self.calculateView.alpha = 1.0
+        UIView.animate(withDuration: animationDuration, delay: 0.0, usingSpringWithDamping: 1.0, initialSpringVelocity: 5, options: .curveEaseIn,
+                       animations: { 
+                        self.calculateView.alpha = 0.0
+                        self.calculateView.transform = CGAffineTransform.init(scaleX: 0.3, y: 0.3)
+        }) { finished in
+            if finished { completion?(finished) }
+        }
+    }
+}
+
+/// MARK: - UICalculateView Delegation
+
+extension UICalculateViewController: UICalculateViewDelegate {
+    func calculateWasTapped(for: UICalculateView, score: String, total: String) {
+        let totalD = Double(total) ?? 1.0
+        let totalSafe = totalD > 0.0 ? totalD : 1.0
+        let roundingAmount = UserDefaults.standard.integer(forKey: kUserDefaultRoundingAmount)
+        let percent = (((Double(score) ?? 0.0) / totalSafe) * 100.0).roundedUpTo(roundingAmount)
+        
+        
+        self.animateOut { [weak self] _ in
+            self?.calculationCompletion(percent)
+        }
+    }
+    
+    func exitButtonWasTapped(for: UICalculateView) {
+        self.animateOut(completion: nil)
+    }
+}
